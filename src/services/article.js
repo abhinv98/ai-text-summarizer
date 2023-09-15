0297f6e5e5ed75fa5bdaf06575662daca4a3a73d
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-09-15 14:40:47 +0530

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const rapidApiKey = import.meta.env.VITE_RAPID_ARTICLE_KEY;
 export const articleApi = createApi({
   reducerPath: "articleApi",
-  baseQuery: {
+  baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
       headers.set("X-RapidAPI-Key", rapidApiKey);
@@ -13,7 +13,7 @@ export const articleApi = createApi({
       );
       return headers;
     },
-  },
+  }),
   endpoints: (builder) => ({
     getSummary: builder.query({
       // encodeURIComponent() function encodes special characters that may be present in the parameter values
